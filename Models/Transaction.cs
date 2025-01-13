ce7e54272a49748e26eea74ddfa6aa33e63863bc
Itsuki06 <trinhtung0604@gmail.com>
2025-01-13 10:23:27 +0700

    + Add CONNECTION STRING + NuGet Console: Add-Migration "Initial Create" + NuGet Console: Update-Database

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -8,7 +8,6 @@ namespace TestWebApplication.Models
         [Key]
         public int TransactionId { get; set; }
 
-        // CategoryId
         public int CategoryId { get; set; }
         public Category Category { get; set; }
         public int Amount { get; set; }
