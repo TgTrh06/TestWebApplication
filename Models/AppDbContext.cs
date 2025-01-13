8bc857736c2cb70a6a34f886e4c04ec66923ae14
Itsuki06 <trinhtung0604@gmail.com>
2025-01-13 07:38:07 +0700

    Add project files.

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestWebApplication.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
